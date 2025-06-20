fea33710a7875445e4375acb605b983b1a10b6da
Sylvain <sylvain.maurier@gmail.com>
2025-06-20 09:05:21 +0200

    sandbox

---
+++ 04 - Data types/05.Array methods/sandbox.js
@@ -0,0 +1,28 @@
+// let arrayTest = ["a", "b", "c", "d"];
+// console.log(arrayTest)
+// arrayTest.pop();
+// console.log(arrayTest)
+// arrayTest.push("pusha", "pushb", "pushc");
+// console.log(arrayTest)
+// arrayTest.shift()
+// console.log(arrayTest)
+// arrayTest.unshift("unshifta", "unshiftb")
+// console.log(arrayTest)
+
+// delete (arrayTest[0])
+// console.log(arrayTest)
+
+// let arrayTest = ["Hello", "My", "name", "is", "Bob"];
+// console.log(arrayTest)
+// arrayTest.splice(1, 1) // Index 1 , On retire 1 element, pas d'element ajouté car pas de param a suivre
+// console.log(arrayTest)
+// arrayTest.splice(1, 0, "My", "fkn")
+// console.log(arrayTest)
+// arrayTest.splice(-1, 1, "Sylvain")
+// console.log(arrayTest)
+
+// let sliceTest = ["t", "e", "s", "t"];
+// let slicedArray = sliceTest.slice(1, 10);
+// console.log(slicedArray);
+
+let arrayToConcat = [1, 2]
